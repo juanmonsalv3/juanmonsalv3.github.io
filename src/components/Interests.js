4e0d48fbd57f37eeae360fd116b650e75075ff4b
Juan Monsalve <j.monsalve@astoundcommerce.com>
2023-02-14 17:30:38 -0500

    fix interests

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -5,11 +5,11 @@ const list = ['Coding', 'Football', 'Reading', 'Video Games'];
 
 const Interest = ({ label }) => <li className='mb-4'>{label}</li>;
 
-const Interests = () => {
+const Interests = ({className}) => {
   const { t } = useTranslation();
 
   return (
-    <Section header={t('interests')}>
+    <Section header={t('interests')} className={className}>
       <article className='relative basis-6/12 shrink-0 pr-20'>
         <ul className='list-disc list-outside pl-4 mt-4 text-sm text-justify'>
           {list.map((l) => (
