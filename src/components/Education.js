eb2217f490ca4219342ae6efbc5bd1d35b086ccf
Juan Monsalve <j.monsalve@astoundcommerce.com>
2023-02-14 22:12:02 -0500

    fix fonts on mobile

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,8 +5,8 @@ const Education = ({ className }) => {
   const { t } = useTranslation();
   return (
     <Section header={t('education')} className={className}>
-      <article className='relative basis-6/12 shrink-0 pr-20'>
-        <h3 className='text-xl font-bold text-slate-100 border-b-white border-b-2'>
+      <article className='relative basis-6/12 shrink-0 pr-20 max-md:pr-0'>
+        <h3 className='text-md font-bold text-slate-100 border-b-white border-b-2'>
           Engineering on systems and informatics
         </h3>
         <h4 className='text-md text-slate-200'>Universidad de Cartagena</h4>
